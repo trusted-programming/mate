4e37046976f81664fb181bca70feb3eab8c89986
lucarlig <luca.carlig@huawei.com>
2023-12-05 15:24:50 +0000

    progress but lib wont compile because rustc is running with wrong version

--- README.md
+++ README.md
@@ -5,10 +5,50 @@ There are 3 sections -- `rules`, `safety`, and `parallel`:
 - the goal of `safety` is to detect opportunities to convert `unsafe` code into safe ones;
 - the goal of `parallel` is to lint all the places where there is a possibility to parallelize the code. 
 
+### installation
+
+1. clone the project
+    ```
+    git clone git@github.com:trusted-programming/cargo-mate.git
+    cd cargo-mate
+    ```
+2. cargo install it 
+    ```
+    cargo install --path .
+    ```
+3. copy libs files in your toolchain cargo-mate is dynamically linked to 2 rust nightly libs, before the tool can work it need
+    ```
+    cp ~/.rustup/toolchains/<TOOLCHAIN nightly-2023-08-24>/lib/librustc_driver-df8e10c587cf8daa.dylib ~/.rustup/toolchains/<TOOLCHAIN of project>/lib/
+    cp ~/.rustup/toolchains/<TOOLCHAIN nightly-2023-08-24>/lib/libstd-bff7f270c7778e6c.dylib ~/.rustup/toolchains/<TOOLCHAIN of project>/lib/
+
+    Example for apple silicon Mac on project running on the stable toolchain:
+    cp ~/.rustup/toolchains/nightly-2023-08-24-aarch64-apple-darwin/lib/librustc_driver-df8e10c587cf8daa.dylib ~/.rustup/toolchains/stable-aarch64-apple-darwin/lib/
+    cp ~/.rustup/toolchains/nightly-2023-08-24-aarch64-apple-darwin/lib/libstd-bff7f270c7778e6c.dylib ~/.rustup/toolchains/stable-aarch64-apple-darwin/lib/
+    ```
+4. run `cargo mate` by default it will run the parallel suggestions
+
+### update to latest
+1. clone the project
+    ```
+    git clone git@github.com:trusted-programming/cargo-mate.git
+    or
+    cd cargo-mate
+    git pull
+    ```
+2. cargo install it using force to overwrite previous
+    ```
+    cargo install --path . --force
+    ```
+3. if libs are missing copy in your toolchain
+
+
 ### benchmarks
 -  https://github.com/alexcrichton/tar-rs
 -  https://gitee.com/openharmony/commonlibrary_rust_ylong_runtime
 -  https://gitee.com/organizations/openharmony/projects?lang=Rust
+
+
+## Parallel list
 
 ## Rules list
 1. non_snake_case
@@ -46,5 +86,5 @@ There are 3 sections -- `rules`, `safety`, and `parallel`:
 
 ## Unsafe to Safe list
 
-## Parallel list
+
 
--- src/main.rs
+++ src/main.rs
@@ -48,54 +48,49 @@ fn main() -> Result<()> {
     let category = args.category;
 
     for target in targets {
-        let mut rustc_args = Vec::new();
-        let src_path = target.src_path().path().map_or(String::new(), |path| {
-            path.to_str().unwrap_or_default().to_string()
-        });
+        if target.is_bin() || target.is_lib() {
+            let mut rustc_args = Vec::new();
+            let src_path = target.src_path().path().map_or(String::new(), |path| {
+                path.to_str().unwrap_or_default().to_string()
+            });
+            rustc_args.push(String::new());
+            rustc_args.push(src_path);
 
-        rustc_args.push(String::new());
-        rustc_args.push(src_path);
+            // List all files in the deps directory
+            for entry in fs::read_dir(&deps_directory)? {
+                let entry = entry?;
+                let path = entry.path();
 
-        // List all files in the deps directory
-        for entry in fs::read_dir(&deps_directory)? {
-            let entry = entry?;
-            let path = entry.path();
+                // Check if the file is an rlib
+                if let Some(extension) = path.extension() {
+                    if extension == "rlib" {
+                        let path_string = path.to_string_lossy().into_owned();
 
-            // Check if the file is an rlib
-            if let Some(extension) = path.extension() {
-                if extension == "rlib" {
-                    let path_string = path.to_string_lossy().into_owned();
-
-                    rustc_args.push("--extern".to_string());
-                    rustc_args.push(path_string);
+                        rustc_args.push("--extern".to_string());
+                        rustc_args.push(path_string);
+                    }
                 }
             }
-        }
-        dbg!(&rustc_args);
+            dbg!(&rustc_args);
 
-        if target.is_bin() || target.is_lib() {
-            for category in &category {
-                let _ignore_errors = match category {
-                    Category::Parallel => {
-                        with_lints(&rustc_args, vec![], |store: &mut LintStore| {
-                            store.register_late_pass(|_| Box::new(phase2::simple::FilterSimple));
-                            store
-                                .register_late_pass(|_| Box::new(phase3::fold::simple::FoldSimple));
-                            store.register_late_pass(|_| {
-                                Box::new(phase4::fold::simple::ParFoldSimple)
-                            });
-                            store.register_early_pass(|| Box::new(phase1::ForEach));
-                        })
-                    }
-                    Category::Rules => with_lints(&rustc_args, vec![], |store: &mut LintStore| {
-                        store.register_late_pass(|_| Box::new(DefaultNumericFallback));
-                        store.register_late_pass(|_| Box::new(MissingDebugImplementations));
-                    }),
-                    Category::Safety => {
-                        with_lints(&rustc_args, vec![], |_store: &mut LintStore| {})
-                    }
-                };
-            }
+            let errors = with_lints(&rustc_args, vec![], |store: &mut LintStore| {
+                store.register_late_pass(|_| Box::new(phase2::simple::FilterSimple));
+                store.register_late_pass(|_| Box::new(phase3::fold::simple::FoldSimple));
+                store.register_late_pass(|_| Box::new(phase4::fold::simple::ParFoldSimple));
+                store.register_early_pass(|| Box::new(phase1::ForEach));
+            });
+            // for category in &category {
+            //     let _ignore_errors = match category {
+            //         Category::Parallel => {}
+            //         Category::Rules => with_lints(&rustc_args, vec![], |store: &mut LintStore| {
+            //             store.register_late_pass(|_| Box::new(DefaultNumericFallback));
+            //             store.register_late_pass(|_| Box::new(MissingDebugImplementations));
+            //         }),
+            //         Category::Safety => {
+            //             with_lints(&rustc_args, vec![], |_store: &mut LintStore| {})
+            //         }
+            //     };
+            // }
         }
     }
 
