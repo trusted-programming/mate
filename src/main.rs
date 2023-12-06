d2feb482e4ecb57c7ae969d2872be01c1438fd1b
lucarlig <luca.carlig@huawei.com>
2023-12-06 09:39:40 +0000

    latest with piping of stdout (broken)

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,27 @@ use cargo::ops::{self, CompileOptions};
 use cargo::util::command_prelude::CompileMode;
 use cargo::util::important_paths::find_root_manifest_for_wd;
 use cargo::util::Config;
-use cargo_cli::{Category, Cli};
-use clap::Parser;
+// use cargo_cli::Cli;
+// use clap::Parser;
 use lints::parallel::{phase1, phase2, phase3, phase4};
-use lints::rules::default_numeric_fallback::DefaultNumericFallback;
-use lints::rules::missing_debug_implementations::MissingDebugImplementations;
+// use lints::rules::default_numeric_fallback::DefaultNumericFallback;
+// use lints::rules::missing_debug_implementations::MissingDebugImplementations;
+use lazy_static::lazy_static;
 use rustc_lint::LintStore;
-use rustc_tools::with_lints;
+use rustc_tools::with_lints_and_error_output;
+use rustfix::{apply_suggestions, get_suggestions_from_json, Filter, Suggestion};
+use std::collections::HashSet;
 use std::fs;
+use std::io::{self, Read, Write};
 use std::path::PathBuf;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref BUFFER: Mutex<Vec<u8>> = Mutex::new(Vec::new());
+}
 
 fn main() -> Result<()> {
-    let Cli::Mate(args) = Cli::parse();
+    // let Cli::Mate(args) = Cli::parse();
 
     let config = Config::default()?;
     let manifest_path = find_root_manifest_for_wd(config.cwd())?;
@@ -45,7 +54,7 @@ fn main() -> Result<()> {
     let build_mode = "debug"; // Change to "release" if needed
     let deps_directory = target_directory.join(build_mode).join("deps");
 
-    let category = args.category;
+    // let category = args.category;
 
     for target in targets {
         if target.is_bin() || target.is_lib() {
@@ -71,14 +80,41 @@ fn main() -> Result<()> {
                     }
                 }
             }
-            dbg!(&rustc_args);
 
-            let errors = with_lints(&rustc_args, vec![], |store: &mut LintStore| {
-                store.register_late_pass(|_| Box::new(phase2::simple::FilterSimple));
-                store.register_late_pass(|_| Box::new(phase3::fold::simple::FoldSimple));
-                store.register_late_pass(|_| Box::new(phase4::fold::simple::ParFoldSimple));
-                store.register_early_pass(|| Box::new(phase1::ForEach));
-            });
+            // Redirect stdout
+            let original_stdout = io::stdout();
+            let mut lock = original_stdout.lock();
+            let mut buffer = io::Cursor::new(Vec::new());
+            io::set_print(Some(Box::new(buffer.clone())));
+
+            let error_output = "JSON";
+
+            // Run the function that includes the compiler execution
+            with_lints_and_error_output(
+                &rustc_args,
+                vec![],
+                error_output,
+                |store: &mut LintStore| {
+                    store.register_late_pass(|_| Box::new(phase2::simple::FilterSimple));
+                    store.register_late_pass(|_| Box::new(phase3::fold::simple::FoldSimple));
+                    store.register_late_pass(|_| Box::new(phase4::fold::simple::ParFoldSimple));
+                    store.register_early_pass(|| Box::new(phase1::ForEach));
+                },
+            )?;
+
+            // Read from buffer
+            let mut output = String::new();
+            buffer.set_position(0); // Reset cursor to the start
+            buffer.read_to_string(&mut output)?;
+
+            let only = HashSet::new();
+            // Step 2: Parse JSON output to get suggestions
+            let suggestions: Vec<Suggestion> =
+                get_suggestions_from_json(&output, &only, Filter::MachineApplicableOnly)?;
+
+            // Step 3: Apply suggestions
+            apply_suggestions(&output, &suggestions)?;
+
             // for category in &category {
             //     let _ignore_errors = match category {
             //         Category::Parallel => {}
