619b1a9e7a40e7581d2b04d430d9338b9e6deda0
lucarlig <luca.carlig@huawei.com>
2023-12-12 15:00:01 +0000

    cargo fmt

--- src/lints/phase1/mod.rs
+++ src/lints/phase1/mod.rs
@@ -1,5 +1,5 @@
 use rustc_ast::ast::{Expr, ExprKind};
-use rustc_ast::visit::{Visitor, walk_expr};
+use rustc_ast::visit::{walk_expr, Visitor};
 use rustc_errors::Applicability;
 use rustc_lint::{EarlyContext, EarlyLintPass, LintContext};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
@@ -26,16 +26,16 @@ struct Validator {
 impl Visitor<'_> for Validator {
     fn visit_expr(&mut self, ex: &Expr) {
         match &ex.kind {
-            ExprKind::Try(_) 
-                | ExprKind::Ret(_) 
-                | ExprKind::Break(_, _) => self.has_early_ret = true,
+            ExprKind::Try(_) | ExprKind::Ret(_) | ExprKind::Break(_, _) => {
+                self.has_early_ret = true
+            }
             ExprKind::Await(e, _) => {
                 self.is_async = true;
                 self.visit_expr(&e)
             }
             _ => walk_expr(self, ex),
         }
-    }    
+    }
 }
 
 declare_lint_pass!(ForEach => [WARN_FOREACH]);
@@ -44,10 +44,15 @@ impl EarlyLintPass for ForEach {
         // Match on for loop expressions
         if let ExprKind::ForLoop(pat, iter, block, _) = &expr.kind {
             // Make sure we ignore cases that require a try_foreach
-            let mut cft = Validator { has_early_ret: false, is_async: false };
+            let mut cft = Validator {
+                has_early_ret: false,
+                is_async: false,
+            };
             cft.visit_block(&block);
             cft.visit_expr(&iter);
-            if cft.has_early_ret || cft.is_async { return }
+            if cft.has_early_ret || cft.is_async {
+                return;
+            }
 
             let src_map = cx.sess().source_map();
             let iter_snip = src_map.span_to_snippet(iter.span).unwrap();
@@ -59,7 +64,10 @@ impl EarlyLintPass for ForEach {
 
             // Assumes into_iter can be applied, more checks can be done to see if iter or iter_mut
             // may apply.
-            let suggestion = format!("({}).into_iter().for_each(|{}| {});", iter_snip, pat_snip, block_snip);
+            let suggestion = format!(
+                "({}).into_iter().for_each(|{}| {});",
+                iter_snip, pat_snip, block_snip
+            );
 
             cx.struct_span_lint(
                 WARN_FOREACH,
