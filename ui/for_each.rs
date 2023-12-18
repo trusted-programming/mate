017c7496e0bcab08d745f15f9932c757c9c781c0
lucarlig <luca.carlig@huawei.com>
2023-12-18 11:07:48 +0000

    expand for each test

--- ui/for_each.rs
+++ ui/for_each.rs
@@ -1,11 +1,27 @@
 // run-rustfix
 #[allow(unused_imports)]
 use rayon::prelude::*;
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     for _ in 1..=100 {
         thread::sleep(Duration::from_secs(1));
+
+        let _res = build_request_builder();
     }
+}
+
+fn build_request_builder() -> (String, String) {
+    let mut a_map = HashMap::new();
+    a_map.insert("a".to_string(), "b".to_string());
+    a_map.insert("c".to_string(), "d".to_string());
+    a_map.insert("e".to_string(), "f".to_string());
+
+    let mut request = (String::new(), String::new());
+    for (key, value) in a_map.iter() {
+        request = (key.clone(), value.clone());
+    }
+    request
 }
