dc01b3e661cbf4687160f9f441fd83eeace0fb34
lucarlig <luca.carlig@huawei.com>
2023-12-19 10:22:55 +0000

    add more tests

--- ui/for_each.rs
+++ ui/for_each.rs
@@ -6,11 +6,10 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    for _ in 1..=100 {
-        thread::sleep(Duration::from_secs(1));
-
-        let _res = build_request_builder();
-    }
+    just_loop();
+    build_request_builder();
+    // loop_continue();
+    // nested_loop();
 }
 
 fn build_request_builder() -> (String, String) {
@@ -25,3 +24,31 @@ fn build_request_builder() -> (String, String) {
     }
     request
 }
+
+fn just_loop() {
+    for _ in 1..=100 {
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+// fn loop_continue() {
+//     let vec_a = vec![1, 2, 3];
+
+//     for a in vec_a {
+//         if a == 1 {
+//             continue;
+//         }
+//         dbg!(a);
+//     }
+// }
+
+// fn nested_loop() {
+//     let vec_a = vec![1, 2, 3];
+//     let vec_b = vec![1, 2, 3];
+
+//     for a in vec_a {
+//         for b in &vec_b {
+//             dbg!(a, b);
+//         }
+//     }
+// }
