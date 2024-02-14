75f40e466ecb88764f2ba984b8eb4dbe3fb7c979
lucarlig <luca.carlig@huawei.com>
2024-02-14 12:31:53 +0400

    cleanup code in par_iter

--- lints/par_iter/src/lib.rs
+++ lints/par_iter/src/lib.rs
@@ -4,6 +4,7 @@
 
 extern crate rustc_errors;
 extern crate rustc_hir;
+extern crate rustc_middle;
 extern crate rustc_span;
 
 use clippy_utils::{get_trait_def_id, ty::implements_trait};
@@ -14,8 +15,8 @@ use rustc_hir::{
     Expr, ExprKind, Node,
 };
 use rustc_lint::{LateContext, LateLintPass, LintContext};
-use rustc_span::sym;
-use utils::span_to_snippet_macro;
+use rustc_middle::ty::Ty;
+use rustc_span::{sym, Symbol};
 
 dylint_linting::declare_late_lint! {
     /// ### What it does
@@ -38,126 +39,45 @@ dylint_linting::declare_late_lint! {
     "suggest using par iter"
 }
 
-struct ClosureVisitor<'a, 'tcx> {
-    cx: &'a LateContext<'tcx>,
-    is_valid: bool,
-}
-
-struct Validator<'a, 'tcx> {
-    cx: &'a LateContext<'tcx>,
-    is_valid: bool,
-}
-impl<'a, 'tcx> Visitor<'_> for ClosureVisitor<'a, 'tcx> {
-    fn visit_expr(&mut self, ex: &Expr) {
-        match ex.kind {
-            ExprKind::Path(path) => {
-                let res: Res = self.cx.typeck_results().qpath_res(&path, ex.hir_id);
-
-                if let Res::Local(hir_id) = res {
-                    let parent = self.cx.tcx.hir().get_parent(hir_id);
-                    match parent {
-                        Node::Local(local) => {
-                            if let Some(expr) = local.init {
-                                let ty =
-                                    self.cx.tcx.typeck(expr.hir_id.owner).node_type(expr.hir_id);
-                                let implements_send = self
-                                    .cx
-                                    .tcx
-                                    .get_diagnostic_item(sym::Send)
-                                    .map_or(false, |id| implements_trait(self.cx, ty, id, &[]));
-                                let implements_sync = self
-                                    .cx
-                                    .tcx
-                                    .get_diagnostic_item(sym::Sync)
-                                    .map_or(false, |id| implements_trait(self.cx, ty, id, &[]));
-                                let is_copy: bool = self
-                                    .cx
-                                    .tcx
-                                    .get_diagnostic_item(sym::Copy)
-                                    .map_or(false, |id| implements_trait(self.cx, ty, id, &[]));
-                                let valid = is_copy || (implements_send && implements_sync);
-                                self.is_valid = self.is_valid && valid;
-                            };
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => walk_expr(self, ex),
-        }
-    }
-}
-
-impl<'a, 'tcx> Visitor<'_> for Validator<'a, 'tcx> {
-    fn visit_expr(&mut self, ex: &Expr) {
-        match ex.kind {
-            ExprKind::Closure(closure) => {
-                let hir = self.cx.tcx.hir();
-                let node = hir.get(closure.body.hir_id);
-                if let Node::Expr(expr) = node {
-                    let mut closure_visitor = ClosureVisitor {
-                        cx: self.cx,
-                        is_valid: true,
-                    };
-                    closure_visitor.visit_expr(expr);
-                    self.is_valid = self.is_valid && closure_visitor.is_valid;
-                }
-            }
-            _ => walk_expr(self, ex),
-        }
-    }
-}
-
 impl<'tcx> LateLintPass<'tcx> for ParIter {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) {
         if let ExprKind::MethodCall(path, _recv, _args, _span) = &expr.kind {
-            let ident_name = &path.ident.name.to_string()[..];
-            let src_map = cx.sess().source_map();
-            let mut suggestion = span_to_snippet_macro(src_map, expr.span);
-            match ident_name {
-                "into_iter" => suggestion = suggestion.replace("into_iter", "into_par_iter"),
-
-                "iter" => suggestion = suggestion.replace("iter", "par_iter"),
-                "iter_mut" => suggestion = suggestion.replace("iter_mut", "par_iter_mut"),
-                _ => return,
+            let method_name = path.ident.name.to_string();
+            let replacement = get_replacement_method(&method_name);
+            if replacement.is_empty() {
+                return;
             }
+
+            let suggestion = generate_suggestion(cx, expr, &method_name, replacement);
 
             let ty = cx.typeck_results().expr_ty(expr);
 
-            let mut implements_par_iter = false;
-
-            let trait_defs = vec![
-                get_trait_def_id(cx, &["rayon", "iter", "IntoParallelIterator"]),
-                get_trait_def_id(cx, &["rayon", "iter", "ParallelIterator"]),
-                // @todo get_trait_def_id(cx, &["rayon", "iter", "IndexedParallelIterator"]),
-                // @todo get_trait_def_id(cx, &["rayon", "iter", "IntoParallelRefIterator"]),
-                // @todo get_trait_def_id(cx, &["rayon", "iter", "IntoParallelRefMutIterator"]),
+            let trait_paths = [
+                ["rayon", "iter", "IntoParallelIterator"],
+                ["rayon", "iter", "ParallelIterator"],
+                // @todo ["rayon", "iter", "IndexedParallelIterator"],
+                // @todo ["rayon", "iter", "IntoParallelRefIterator"],
+                // @todo ["rayon", "iter", "IntoParallelRefMutIterator"],
+                // Add more traits as needed
             ];
 
-            for t in trait_defs {
-                if let Some(trait_def_id) = t {
-                    implements_par_iter =
-                        implements_par_iter || implements_trait(cx, ty, trait_def_id, &[]);
-                }
-            }
+            let implements_par_iter = trait_paths
+                .iter()
+                .filter_map(|path| get_trait_def_id(cx, path))
+                .any(|trait_def_id| implements_trait(cx, ty, trait_def_id, &[]));
 
             if !implements_par_iter {
                 return;
             }
 
             // check that all types inside the closures are Send and sync or Copy
-            let mut validator = Validator { cx, is_valid: true };
-
             let parent_node = cx.tcx.hir().get_parent(expr.hir_id);
-            match parent_node {
-                Node::Expr(expr) => {
-                    validator.visit_expr(expr);
+            if let Node::Expr(parent_expr) = parent_node {
+                let mut validator = Validator { cx, is_valid: true };
+                validator.visit_expr(parent_expr);
+                if !validator.is_valid {
+                    return;
                 }
-                // Handle other kinds of parent nodes as needed
-                _ => {}
-            }
-            if !validator.is_valid {
-                return;
             }
 
             cx.struct_span_lint(
@@ -174,6 +94,86 @@ impl<'tcx> LateLintPass<'tcx> for ParIter {
             );
         }
     }
+}
+
+struct ClosureVisitor<'a, 'tcx> {
+    cx: &'a LateContext<'tcx>,
+    is_valid: bool,
+}
+
+struct Validator<'a, 'tcx> {
+    cx: &'a LateContext<'tcx>,
+    is_valid: bool,
+}
+
+impl<'a, 'tcx> Visitor<'_> for ClosureVisitor<'a, 'tcx> {
+    fn visit_expr(&mut self, ex: &Expr) {
+        if let ExprKind::Path(ref path) = ex.kind {
+            if let Res::Local(hir_id) = self.cx.typeck_results().qpath_res(&path, ex.hir_id) {
+                if let Node::Local(local) = self.cx.tcx.hir().get_parent(hir_id) {
+                    if let Some(expr) = local.init {
+                        self.is_valid &= is_type_valid(
+                            self.cx,
+                            self.cx.tcx.typeck(expr.hir_id.owner).node_type(expr.hir_id),
+                        );
+                    };
+                }
+            }
+        } else {
+            walk_expr(self, ex)
+        }
+    }
+}
+
+impl<'a, 'tcx> Visitor<'_> for Validator<'a, 'tcx> {
+    fn visit_expr(&mut self, ex: &Expr) {
+        if let ExprKind::Closure(closure) = ex.kind {
+            if let Node::Expr(expr) = self.cx.tcx.hir().get(closure.body.hir_id) {
+                let mut closure_visitor = ClosureVisitor {
+                    cx: self.cx,
+                    is_valid: true,
+                };
+                closure_visitor.visit_expr(expr);
+                self.is_valid &= closure_visitor.is_valid;
+            }
+        } else {
+            walk_expr(self, ex)
+        }
+    }
+}
+fn is_type_valid<'tcx>(cx: &LateContext<'tcx>, ty: Ty<'tcx>) -> bool {
+    let implements_send = check_trait_impl(cx, ty, sym::Send);
+    let implements_sync = check_trait_impl(cx, ty, sym::Sync);
+    let is_copy = check_trait_impl(cx, ty, sym::Copy);
+    is_copy || (implements_send && implements_sync)
+}
+
+fn check_trait_impl<'tcx>(cx: &LateContext<'tcx>, ty: Ty<'tcx>, trait_name: Symbol) -> bool {
+    cx.tcx
+        .get_diagnostic_item(trait_name)
+        .map_or(false, |trait_id| implements_trait(cx, ty, trait_id, &[]))
+}
+
+fn get_replacement_method(method_name: &str) -> &str {
+    match method_name {
+        "into_iter" => "into_par_iter",
+        "iter" => "par_iter",
+        "iter_mut" => "par_iter_mut",
+        _ => "",
+    }
+}
+
+fn generate_suggestion(
+    cx: &LateContext<'_>,
+    expr: &Expr<'_>,
+    method_name: &str,
+    replacement: &str,
+) -> String {
+    cx.sess()
+        .source_map()
+        .span_to_snippet(expr.span)
+        .map(|s| s.replace(method_name, replacement))
+        .unwrap_or_else(|_| String::from("/* error: unable to generate suggestion */"))
 }
 
 #[test]
