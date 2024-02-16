cefad5d71a3387441a418fcfb798e6f29589facd
lucarlig <luca.carlig@huawei.com>
2024-02-16 23:22:30 +0400

    add script file instead of everything in CI
    update counter
    
    add debug info
    
    debug info
    
    debug info more
    
    change rs files

--- scripts/counters.sh
+++ scripts/counters.sh
@@ -1,52 +1,62 @@
-for_loop_count=0
+#!/bin/bash
 
-find . -type f -name '*.rs' | while read -r file; do
-    echo "Checking $file"
+rs_files=$(find . -type f -name '*.rs')
+
+for_loop_count=0
+echo "### COUNTING FOR LOOPS ###"
+for file in "${rs_files[@]}"; do
+    echo "Checking: $file"
     for_loop_count_in_file=$(grep -oE 'for\s+(\([^)]+\)|\w+)\s+in\s+[^{]+' "$file" | wc -l)
     echo "Count in file: $for_loop_count_in_file"
     ((for_loop_count += for_loop_count_in_file))
 done
-echo "for loop occurrences: $for_loop_count"
 
 iter_count=0
 iter_mut_count=0
 into_iter_count=0
 
-# Iterate over all .rs files in the current directory and subdirectories
-find . -type f -name '*.rs' | while read -r file; do
-    echo checking $file
-    # Count occurrences of iterator methods in the current file
+echo "### COUNTING ITERS ###"
+for file in "${rs_files[@]}"; do
+    echo "Checking: $file"
     iter_count_in_file=$(grep -o '\.iter()' "$file" | wc -l)
     iter_mut_count_in_file=$(grep -o '\.iter_mut()' "$file" | wc -l)
     into_iter_count_in_file=$(grep -o '\.into_iter()' "$file" | wc -l)
+    echo "iter_count_in_file: $iter_count_in_file"
+    echo "iter_mut_count_in_file: $iter_mut_count_in_file"
+    echo "into_iter_count_in_file: $into_iter_count_in_file"
 
-    # Update total counters
     ((iter_count += iter_count_in_file))
     ((iter_mut_count += iter_mut_count_in_file))
     ((into_iter_count += into_iter_count_in_file))
 done
 
-# Print results
-echo ".iter() occurrences: $iter_count"
-echo ".iter_mut() occurrences: $iter_mut_count"
-echo ".into_iter() occurrences: $into_iter_count"
-
 par_iter_count=0
 into_par_iter_count=0
 par_iter_mut_count=0
 
-# Iterate over all .rs files in the current directory and subdirectories
-find . -type f -name '*.rs' | while read -r file; do
-    echo checking $file
+echo "### COUNTING PAR ITERS ###"
+for file in "${rs_files[@]}"; do
+    echo "Checking: $file"
     par_iter_count_in_file=$(grep -o '\.par_iter()' "$file" | wc -l)
     into_par_iter_count_in_file=$(grep -o '\.into_par_iter()' "$file" | wc -l)
     par_iter_mut_count_in_file=$(grep -o '\.par_iter_mut()' "$file" | wc -l)
-
+    echo "par_iter_count_in_file: $par_iter_count_in_file"
+    echo "into_par_iter_count_in_file: $into_par_iter_count_in_file"
+    echo "par_iter_mut_count_in_file: $par_iter_mut_count_in_file"
     ((par_iter_count += par_iter_count_in_file))
     ((into_par_iter_count += into_par_iter_count_in_file))
     ((par_iter_mut_count += par_iter_mut_count_in_file))
 done
 
+echo
+echo "### FINAL RESULTS ###"
+echo
+echo "for loop occurrences: $for_loop_count"
+echo ".iter() occurrences: $iter_count"
+echo ".iter_mut() occurrences: $iter_mut_count"
+echo ".into_iter() occurrences: $into_iter_count"
 echo ".par_iter() occurrences: $par_iter_count"
 echo ".into_par_iter() occurrences: $into_par_iter_count"
 echo ".par_iter_mut() occurrences: $par_iter_mut_count"
+echo
+echo "### ALL DONE ###"
