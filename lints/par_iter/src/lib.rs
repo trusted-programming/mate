de3cf1781d756bf5921cf79e40580fe04b860d88
lucarlig <luca.carlig@huawei.com>
2024-03-04 14:00:19 +0000

    add checks that methods exists on parallel iterator

--- lints/par_iter/src/lib.rs
+++ lints/par_iter/src/lib.rs
@@ -2,6 +2,7 @@
 #![warn(unused_extern_crates)]
 #![feature(let_chains)]
 
+extern crate rustc_data_structures;
 extern crate rustc_errors;
 extern crate rustc_hash;
 extern crate rustc_hir;
@@ -14,12 +15,13 @@ mod constants;
 mod utils;
 mod variable_check;
 
-use clippy_utils::get_parent_expr;
+use clippy_utils::{get_parent_expr, get_trait_def_id};
+use rustc_data_structures::fx::FxHashSet;
 use rustc_errors::Applicability;
-use rustc_hir as hir;
 use rustc_hir::intravisit::Visitor;
+use rustc_hir::{self as hir};
 use rustc_lint::{LateContext, LateLintPass, LintContext};
-use rustc_middle::ty::{ty_kind::TyKind, Ty};
+use rustc_middle::ty::{self, ty_kind::TyKind, Ty};
 use utils::{check_implements_par_iter, generate_suggestion, is_type_valid};
 use variable_check::check_variables;
 
@@ -54,11 +56,53 @@ impl<'tcx> LateLintPass<'tcx> for ParIter {
         {
             let ty = cx.typeck_results().expr_ty(recv);
 
-            if !check_implements_par_iter(cx, recv).is_empty() && is_type_valid(cx, ty) {
+            let par_iter_traits = check_implements_par_iter(cx, recv);
+            if !par_iter_traits.is_empty() && is_type_valid(cx, ty) {
+                // TODO: issue with into_par_iter() need to check directly with
+                // parallel iterator
+                // let mut implemented_methods: Vec<&AssocItems> = Vec::new();
+
+                let mut allowed_methods: FxHashSet<&str> = FxHashSet::default();
+                allowed_methods.insert("into_iter");
+                allowed_methods.insert("iter");
+                allowed_methods.insert("iter_mut");
+
+                if let Some(parallel_iterator_def_id) =
+                    get_trait_def_id(cx, &["rayon", "iter", "ParallelIterator"])
+                    && let Some(parallel_indexed_iterator_def_id) =
+                        get_trait_def_id(cx, &["rayon", "iter", "IndexedParallelIterator"])
+                {
+                    let parallel_iterator_associated_items =
+                        cx.tcx.associated_items(parallel_iterator_def_id);
+                    // Filter out only methods from the associated items
+                    let parallel_iterator_methods: Vec<_> = parallel_iterator_associated_items
+                        .in_definition_order()
+                        .filter(|item| matches!(item.kind, ty::AssocKind::Fn))
+                        .map(|item| item.name.as_str())
+                        .collect();
+
+                    let parallel_indexed_iterator_associated_items =
+                        cx.tcx.associated_items(parallel_indexed_iterator_def_id);
+                    // Filter out only methods from the associated items
+                    let parallel_indexed_iterator_methods: Vec<_> =
+                        parallel_indexed_iterator_associated_items
+                            .in_definition_order()
+                            .filter(|item| matches!(item.kind, ty::AssocKind::Fn))
+                            .map(|item| item.name.as_str())
+                            .collect();
+                    allowed_methods.extend(parallel_iterator_methods);
+                    allowed_methods.extend(parallel_indexed_iterator_methods);
+                } else {
+                    return;
+                }
+
                 let mut top_expr = *recv;
 
                 while let Some(parent_expr) = get_parent_expr(cx, top_expr) {
-                    if let hir::ExprKind::MethodCall(_, _, _, _) = parent_expr.kind {
+                    if let hir::ExprKind::MethodCall(method_name, _, _, _) = parent_expr.kind {
+                        if !allowed_methods.contains(method_name.ident.as_str()) {
+                            return;
+                        }
                         top_expr = parent_expr;
                     } else {
                         break;
@@ -171,6 +215,9 @@ impl<'a, 'tcx> hir::intravisit::Visitor<'_> for ExprVisitor<'a, 'tcx> {
 impl<'a, 'tcx> hir::intravisit::Visitor<'_> for Validator<'a, 'tcx> {
     fn visit_expr(&mut self, ex: &hir::Expr) {
         if let hir::ExprKind::MethodCall(_method_name, _receiver, args, _span) = ex.kind {
+            if !self.is_valid {
+                return;
+            }
             for arg in args {
                 let mut expr_visitor = ExprVisitor {
                     cx: self.cx,
