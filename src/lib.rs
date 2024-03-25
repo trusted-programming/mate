53ab0e432cbcfc367bcd9fc3d8eb36b1dcaec3c3
lucarlig <luca.carlig@huawei.com>
2024-03-25 09:08:12 +0000

    use new lint ordering

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ extern crate rustc_session;
 #[allow(clippy::no_mangle_with_rust_abi)]
 #[no_mangle]
 pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
+    // PHASE 0
+    rayon_imports::register_lints(sess, lint_store);
     // PHASE 1
     for_each::register_lints(sess, lint_store);
     // PHASE 2
@@ -17,8 +19,5 @@ pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint
     fold::register_lints(sess, lint_store);
     // PHASE 4
     par_fold::register_lints(sess, lint_store);
-
-    // INDEPENDENT LINTS
     par_iter::register_lints(sess, lint_store);
-    rayon_imports::register_lints(sess, lint_store);
 }
